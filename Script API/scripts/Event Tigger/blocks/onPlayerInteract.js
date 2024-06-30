264a27f8eca92649a0297bebba666e5b9507212b
MCFirsting <86946978+FPGTV@users.noreply.github.com>
2024-07-01 00:30:00 +0700

    Update onPlayerInteract.js

--- Script API/scripts/Event Tigger/blocks/onPlayerInteract.js
+++ Script API/scripts/Event Tigger/blocks/onPlayerInteract.js
@@ -1,1 +1,21 @@
-import
+import { world, BlockPermutation, ItemStack } from '@minecraft/server'
+
+world.beforeEvents.worldInitialize.subscribe(initEvent => {
+    initEvent.blockTypeRegistry.registerCustomComponent('mc:khomloy', {
+        onPlayerInteract: e => {
+            const { player, block } = e;
+            const equipment = player.getComponent('equippable');
+            const selectedItem = equipment.getEquipment('Mainhand');
+            const khomloyonfire = location.dimension.spawnEntity("mc:khomloy", {
+                x: location.x,
+                y: location.y,
+                z: location.z,
+            });
+            if (selectedItem.typeId === 'minecraft:flint_and_steel') {
+                player.playSound('fire.ignite');
+                block.setType("minecraft:air");
+                khomloyonfire.dimension.spawnEntity("mc:khomloy_onfire")
+            }
+        }
+    });
+});
