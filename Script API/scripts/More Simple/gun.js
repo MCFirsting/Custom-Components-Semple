594983b0ccf6bca2ddea71a9420937eb16150c13
MCFirsting <86946978+FPGTV@users.noreply.github.com>
2024-07-01 00:54:22 +0700

    Update gun.js

--- Script API/scripts/More Simple/gun.js
+++ Script API/scripts/More Simple/gun.js
@@ -3,7 +3,7 @@ onUse: e => {
     const { itemStack: item, source } = e,
     head = source.getHeadLocation(),
     viewVector = source.getViewDirection(),
-    ammo = 'mc:thrown_water_gun',
+    ammo = 'minecraft:fireball',
     direction = { x: head.x + viewVector.x, y: head.y + viewVector.y, z: head.z + viewVector.z },
     power = { x: viewVector.x*0.75, y: viewVector.y*0.75, z: viewVector.z*0.75 },
     damage = item.getComponent("durability").damage,
@@ -11,7 +11,7 @@ onUse: e => {
     inventory = source.getComponent('inventory').container;
 //
     source.dimension.spawnEntity(ammo, direction).getComponent('minecraft:projectile').shoot(power, { owner: source });
-    source.dimension.playSound('bucket.empty_water', source.location);
+    source.dimension.playSound('sound_name', source.location);
     if (damage == durability) {
         inventory.setItem(source.selectedSlotIndex, null)
         source.dimension.playSound("random.break", source.location);
