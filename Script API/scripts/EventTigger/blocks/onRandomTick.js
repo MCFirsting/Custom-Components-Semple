b9c4d3bce295cd368b1c6eb391985f385f7cd6b8
MCFirsting <86946978+MCFirsting@users.noreply.github.com>
2024-12-31 12:22:48 +0700

    Update onRandomTick.js

--- Script API/scripts/EventTigger/blocks/onRandomTick.js
+++ Script API/scripts/EventTigger/blocks/onRandomTick.js
@@ -1,19 +1,19 @@
 import { world } from "@minecraft/server";
 
+world.beforeEvents.worldInitialize.subscribe(initEvent => {
+    initEvent.blockTypeRegistry.registerCustomComponent('minecraft:name', {
+        onRandomTick: cropGrowRandomTick,
+    });
+});
+
+
 function cropGrowRandomTick(event) {
     const age = event.block.permutation.getState('minecaft:crop_age');
     if (age === undefined || typeof age !== 'number') {
         return;
     }
     else if (age === 4) {
-        return; //fully grown
+        return; //full Level
     }
-
     event.block.setPermutation(event.block.permutation.withState('minecaft:crop_age', age + 1));
 }
-
-world.beforeEvents.worldInitialize.subscribe(initEvent => {
-    initEvent.blockTypeRegistry.registerCustomComponent('minecraft:name', {
-        onRandomTick: cropGrowRandomTick,
-    });
-});
