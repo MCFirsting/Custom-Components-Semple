0c159ac8057a023f77b437186eebe72f1ea5e820
MCFirsting <86946978+MCFirsting@users.noreply.github.com>
2025-01-19 22:16:19 +0700

    Delete Script API/scripts directory

--- Script API/scripts/main.js
+++
@@ -1,8 +0,0 @@
-//import another the script files name
-import 'custom'
-
-//import another the script folder name
-import './scripts/EventTigger/onPlayerInteract'
-
-//more script simple
-import './scripts/MoreSimple/soon'
